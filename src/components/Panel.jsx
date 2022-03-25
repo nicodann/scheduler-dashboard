4e1d336503bbe887e6b012e2a3731b22f2a6bc99
Nico Dann <nicodann@gmail.com>
2022-03-25 15:33:59 +0000

    Add selectPanel function to Panel component as onClick prop

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export default class Panel extends Component {
   render() {
-    const {label, value } = this.props;
+    const { id, label, value, onSelect } = this.props;
     console.log("label: ", label, "value: ", value)
 
     return (
       <section 
         className="dashboard__panel"
+        onClick={() => onSelect(id)}
       >
         <h1 className="dashboard__panel-header">{label}</h1>
         <h1 className="dashboard__panel- value">{value}</h1>
